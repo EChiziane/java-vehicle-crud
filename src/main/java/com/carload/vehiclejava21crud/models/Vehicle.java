1124d3913bc3f9314c9ddacdfeffc6b405f613f8
Ednilson Chiziane <eddybruno43@gmail.com>
2025-01-21 11:24:44 +0200

    feat():Modeling Vehicle

---
+++ src/main/java/com/carload/vehiclejava21crud/models/Vehicle.java
@@ -0,0 +1,22 @@
+package com.carload.vehiclejava21crud.models;
+
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.util.UUID;
+
+@Entity
+@Table(name = "vehicles")
+public class Vehicle {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String model;
+    private String manufacturer;
+    private String manufacturedYear;
+    private String licensePlate;
+  private Double dailyRentalPrice;
+}
