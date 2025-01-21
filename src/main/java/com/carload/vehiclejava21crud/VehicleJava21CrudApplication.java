4eda83ee58af7664307f003d6838098200e30dfd
Ednilson Chiziane <eddybruno43@gmail.com>
2025-01-21 11:16:38 +0200

    initial commit

---
+++ src/main/java/com/carload/vehiclejava21crud/VehicleJava21CrudApplication.java
@@ -0,0 +1,13 @@
+package com.carload.vehiclejava21crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleJava21CrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleJava21CrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carload/vehiclejava21crud/VehicleJava21CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carload.vehiclejava21crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleJava21CrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
